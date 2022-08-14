2c1662117dab54998ded593593f010235b1f5bbb
eddy madu <eddymadu90@gmail.com>
2022-08-14 22:57:25 +0100

    chore:completed the last modal

--- src/modals/wizard/eligibility.vue
+++ src/modals/wizard/eligibility.vue
@@ -42,7 +42,7 @@ const submit = () => {
                   </label>
 
                   <p
-                    class="text-[#4E4E4E]text-center font-extrabold text-[11px]"
+                    class="text-[#4E4E4E] text-center font-extrabold text-[11px]"
                   >
                     UPLOAD IMAGE OF PVC
                   </p>
@@ -50,85 +50,111 @@ const submit = () => {
               </div>
             </div>
             <div class="text-[#4E4E4E] space-y-12">
-              <div class="flex space-x-3 font-extrabold text-[11px]">
-                <p class="">Do you own a smart phone?</p>
-                <div class="flex space-x-3">
-                  <p>YES</p>
-                  <label class="container" for="Yes">
-                    <input type="checkbox" class="mr-3" name="Yes" id="Yes" />
-                    <span class="checkmark"></span>
-                  </label>
-                </div>
+              <div class="flex font-extrabold text-[11px]">
+                <p class="w-full">Do you own a smart phone?</p>
+                <div class="w-full flex space-x-3">
+                  <div class="flex space-x-3">
+                    <p>YES</p>
+                    <label class="container" for="Yes">
+                      <input type="checkbox" class="mr-3" name="Yes" id="Yes" />
+                      <span class="checkmark"></span>
+                    </label>
+                  </div>
 
-                <div class="flex space-x-3">
-                  <p>NO</p>
-                  <label class="container" for="no">
-                    <input type="checkbox" class="mr-3" name="no" id="no" />
-                    <span class="checkmark"></span>
-                  </label>
+                  <div class="flex space-x-3">
+                    <p>NO</p>
+                    <label class="container" for="no">
+                      <input type="checkbox" class="mr-3" name="no" id="no" />
+                      <span class="checkmark"></span>
+                    </label>
+                  </div>
                 </div>
               </div>
 
-              <div class="flex w-full space-x-3 font-extrabold text-[11px]">
-                <p class="">
+              <div class="flex w-full font-extrabold text-[11px]">
+                <p class="w-full">
                   Are you able to browse,<br />connect to WhatsApp or<br />
                   Telegram in your Polling Unit
                 </p>
-                <div class="flex space-x-3">
-                  <p>YES</p>
-                  <label class="container" for="yes2">
-                    <input type="checkbox" class="mr-3" name="yes" id="yes2" />
-                    <span class="checkmark"></span>
-                  </label>
-                </div>
-
-                <div class="flex space-x-3">
-                  <p>NO</p>
-                  <label class="container" for="no2">
-                    <input type="checkbox" class="mr-3" name="no" id="no2" />
-                    <span class="checkmark"></span>
-                  </label>
-                </div>
-              </div>
-              <div class="flex w-auto space-x-3 font-extrabold text-[11px]">
-                <p class="">
-                  What party does Peter Obi<br />
-                  belong to?
-                </p>
-                <div class="flex space-x-3 w-1/2 flex-wrap">
+                <div class="w-full flex space-x-3">
                   <div class="flex space-x-3">
-                    <p>APC</p>
-                    <label class="container" for="APC">
-                      <input type="checkbox" class="mr-3" name="APC" id="APC" />
-                      <span class="checkmark"></span>
-                    </label>
-                  </div>
-
-                  <div class="flex space-x-3">
-                    <p>PDP</p>
-                    <label class="container" for="PDP">
-                      <input type="checkbox" class="mr-3" name="PDP" id="PDP" />
-                      <span class="checkmark"></span>
-                    </label>
-                  </div>
-                  <div class="flex space-x-3">
-                    <p>LABOUR</p>
-                    <label class="container" for="LABOUR">
+                    <p>YES</p>
+                    <label class="container" for="yes2">
                       <input
                         type="checkbox"
                         class="mr-3"
-                        name="LABOUR"
-                        id="LABOUR"
+                        name="yes"
+                        id="yes2"
                       />
                       <span class="checkmark"></span>
                     </label>
                   </div>
+
                   <div class="flex space-x-3">
-                    <p>ANPP</p>
-                    <label class="container" for="SDP">
-                      <input type="checkbox" class="mr-3" name="SDP" id="SDP" />
+                    <p>NO</p>
+                    <label class="container" for="no2">
+                      <input type="checkbox" class="mr-3" name="no" id="no2" />
                       <span class="checkmark"></span>
                     </label>
+                  </div>
+                </div>
+              </div>
+              <div class="flex w-full font-extrabold text-[11px]">
+                <p class="w-full">
+                  What party does Peter Obi<br />
+                  belong to?
+                </p>
+                <div class="flex w-full space-x-3">
+                  <div class="flex space-x-3 flex-wrap">
+                    <div class="flex space-x-3">
+                      <p>APC</p>
+                      <label class="container" for="APC">
+                        <input
+                          type="checkbox"
+                          class="mr-3"
+                          name="APC"
+                          id="APC"
+                        />
+                        <span class="checkmark"></span>
+                      </label>
+                    </div>
+
+                    <div class="flex space-x-3">
+                      <p>PDP</p>
+                      <label class="container" for="PDP">
+                        <input
+                          type="checkbox"
+                          class="mr-3"
+                          name="PDP"
+                          id="PDP"
+                        />
+                        <span class="checkmark"></span>
+                      </label>
+                    </div>
+                    <div class="flex space-x-3">
+                      <p>LABOUR</p>
+                      <label class="container" for="LABOUR">
+                        <input
+                          type="checkbox"
+                          class="mr-3"
+                          name="LABOUR"
+                          id="LABOUR"
+                        />
+                        <span class="checkmark"></span>
+                      </label>
+                    </div>
+                    <div class="flex space-x-3">
+                      <p>ANPP</p>
+                      <label class="container" for="SDP">
+                        <input
+                          type="checkbox"
+                          class="mr-3"
+                          name="SDP"
+                          id="SDP"
+                        />
+                        <span class="checkmark"></span>
+                      </label>
+                    </div>
                   </div>
                 </div>
               </div>
