f331f31255dcf291108e8519bcbed4fa21e1931e
eddy madu <eddymadu90@gmail.com>
2022-08-15 12:35:39 +0100

    stated working on responsiveness

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -24,10 +24,14 @@ const showVerify = provide<{ value: boolean }>("showVerify", showVerifyPhone);
     <!-- Main body -->
     <div class="min-h-screen" v-if="showVerifyPhone == false">
       <!-- section 1 -->
-      <section class="relative mb-52">
-        <img src="/images/svg/sectionone.svg" alt="" />
+      <section class="relative lg:mb-52 mb-32 h-full">
+        <img
+          src="/images/svg/sectionone.svg"
+          alt=""
+          class="md:w-auto w-screen md:h-auto h-96"
+        />
         <p
-          class="absolute font-extrabold text-[55px] left-[11%] top-[27%] leading-[70px] text-white"
+          class="absolute font-extrabold lg:text-[55px] text-[30px] lg:left-[11%] left-[10%] top-[29%] lg:leading-[70px] leading-[30px] text-white"
         >
           Finally a Cause<br />
           That’s Worth Our<br />
@@ -48,7 +52,7 @@ const showVerify = provide<{ value: boolean }>("showVerify", showVerifyPhone);
         </div>
         <!-- slider selector -->
         <div
-          class="flex absolute left-[50%] bottom-[20%] text-[11px] space-x-4"
+          class="flex absolute md:left-[50%] left-[40%] md:bottom-[20%] bottom-[34%] text-[11px] space-x-4"
         >
           <p class="textGrey">01</p>
           <p class="appGreenBg h-5 w-5 pl-1 pt-[1px] text-white rounded-full">
@@ -57,50 +61,90 @@ const showVerify = provide<{ value: boolean }>("showVerify", showVerifyPhone);
           <p class="textGrey">03</p>
         </div>
         <!-- absolute card -->
-        <div class="flex absolute bottom-[-37%] left-[10%]">
-          <div class="relative">
-            <img src="/images/svg/cardone.svg" class="w-[19rem]" alt="" />
-            <div class="absolute text-white top-[15%] left-[8%] space-y-20">
-              <div>
-                <p class="text-[12px] tracking-[4px]">BETTER NIGERIA PLAN</p>
-                <p class="font-extrabold text-[30px] leading-[34px]">
-                  Create a country<br />
-                  for very citizen
-                </p>
-              </div>
+        <div class="absolute md:flex md:bottom-[-37%] bottom-[-55%] left-[10%]">
+          <div class="flex">
+            <div class="relative">
+              <img
+                src="/images/svg/cardone.svg"
+                class="md:w-[19rem] w-[30rem]"
+                alt=""
+              />
+              <div
+                class="absolute text-white md:top-[15%] top-[4%] left-[8%] md:space-y-20 space-y-3"
+              >
+                <div>
+                  <p class="md:text-[12px] text-[10px] tracking-[4px]">
+                    BETTER NIGERIA PLAN
+                  </p>
+                  <p
+                    class="font-extrabold md:text-[30px] text-[20px] md:leading-[34px] leading-5"
+                  >
+                    Create a country<br />
+                    for very citizen
+                  </p>
+                </div>
 
-              <button class="appGreenBg flex px-6 py-2 space-x-2">
-                <span class="text-[12px] font-extrabold mt-1 text-white">
-                  SEE THE PLAN</span
+                <button
+                  class="appGreenBg flex md:px-6 px-3 md:py-2 py-2 space-x-2"
                 >
+                  <span
+                    class="md:text-[12px] text-[10px] font-extrabold mt-1 text-white"
+                  >
+                    SEE THE PLAN</span
+                  >
 
-                <img src="/images/png/arrow-right.png" alt="icon" class="" />
-              </button>
+                  <img
+                    src="/images/png/arrow-right.png"
+                    alt="icon"
+                    class="md:w-auto w-4 md:h-auto h-4 md:mt-0 mt-1"
+                  />
+                </button>
+              </div>
+            </div>
+            <!--  -->
+            <div class="relative">
+              <img
+                src="/images/png/flag.png"
+                class="md:w-[19rem] w-[30rem]"
+                alt=""
+              />
+              <div
+                class="absolute text-white md:top-[15%] top-[3%] left-[8%] md:space-y-20 space-y-3"
+              >
+                <div>
+                  <p class="md:text-[12px] text-[10px] tracking-[4px]">
+                    BETTER NIGERIA PLAN
+                  </p>
+                  <p
+                    class="font-extrabold md:text-[30px] text-[20px] md:leading-[34px] leading-5"
+                  >
+                    Political change<br />
+                    through activism
+                  </p>
+                </div>
+
+                <button class="appGreenBg flex px-6 py-2 space-x-2">
+                  <span
+                    class="md:text-[12px] text-[10px] font-extrabold mt-1 text-white"
+                  >
+                    SEE THE PLAN</span
+                  >
+
+                  <img
+                    src="/images/png/arrow-right.png"
+                    alt="icon"
+                    class="md:w-auto w-4 md:h-auto h-4 md:mt-0 mt-1"
+                  />
+                </button>
+              </div>
             </div>
           </div>
-          <!--  -->
-          <div class="relative">
-            <img src="/images/png/flag.png" class="w-[19rem]" alt="" />
-            <div class="absolute text-white top-[15%] left-[8%] space-y-20">
-              <div>
-                <p class="text-[12px] tracking-[4px]">BETTER NIGERIA PLAN</p>
-                <p class="font-extrabold text-[30px] leading-[34px]">
-                  Political change<br />
-                  through activism
-                </p>
-              </div>
-
-              <button class="appGreenBg flex px-6 py-2 space-x-2">
-                <span class="text-[12px] font-extrabold mt-1 text-white">
-                  SEE THE PLAN</span
-                >
-
-                <img src="/images/png/arrow-right.png" alt="icon" class="" />
-              </button>
-            </div>
-          </div>
-          <div class="appRedBg px-14 py-10 space-y-5">
-            <p class="text-[32px] font-extrabold leading-[34px] text-white">
+          <div
+            class="appRedBg md:px-14 px-3 md:py-10 py-6 md:space-y-5 space-y-2"
+          >
+            <p
+              class="md:text-[32px] text-[20px] font-extrabold md:leading-[34px] leading-[22px] text-white"
+            >
               Prosperity Starts Here<br />
               With Us
             </p>
@@ -108,11 +152,11 @@ const showVerify = provide<{ value: boolean }>("showVerify", showVerifyPhone);
               <input
                 type="text"
                 placeholder="ENTER YOUR PHONE NUMBER"
-                class="bg-white text-[10px] px-6 py-5 font-extrabold text-black w-full border-none focus:outline-none outline-none"
+                class="bg-white text-[10px] md:px-6 px-3 md:py-5 py-2 font-extrabold text-black w-full border-none focus:outline-none outline-none"
               />
               <button
                 @click="showVerifyPhone = true"
-                class="bg-black flex px-6 py-3 space-x-2 text-[12px] font-extrabold text-white"
+                class="bg-black flex px-6 py-3 space-x-2 text-[7px] md:text-[12px] font-extrabold text-white"
               >
                 BECOME A VOLUNTEER
               </button>
@@ -122,11 +166,13 @@ const showVerify = provide<{ value: boolean }>("showVerify", showVerifyPhone);
       </section>
 
       <!-- section 2 -->
-      <section class="pt-48 pb-36 px-36">
-        <div class="customGrey flex pt-5">
+      <section class="md:pt-48 pt-32 md:pb-36 pb-20 md:px-36 px-10">
+        <div class="customGrey md:flex space-y-5 md:space-y-0 pt-5">
           <img src="/images/svg/sectiontwocard.svg" class="w-[20rem]" alt="" />
-          <div class="space-y-5">
-            <p class="text-[34px] font-extrabold leading-[39px]">
+          <div class="space-y-5 md-px-0 px-5">
+            <p
+              class="md:text-[34px] text-[30px] font-extrabold leading-8 md:leading-[39px]"
+            >
               Commit to call, chip in to support <br />Peter Obi and Yusuf Datti
             </p>
 
@@ -147,10 +193,14 @@ const showVerify = provide<{ value: boolean }>("showVerify", showVerifyPhone);
         </div>
       </section>
       <!-- section 3 -->
-      <section class="pb-36">
-        <div class="px-36">
-          <div class="space-y-4">
-            <p class="text-[44px] font-extrabold">The Save Nigeria Agenda</p>
+      <section class="md:pb-36 pb-20">
+        <div class="md:px-36 px-10">
+          <div class="space-y-4 md:px-0 px-5">
+            <p
+              class="md:text-[44px] text-[30px] leading-8 md:leading-[39px] font-extrabold"
+            >
+              The Save Nigeria Agenda
+            </p>
             <p class="text-[12px] text-[#7F7F7F]">
               Nigerians remain hopeful for a national rebirth, which implies
               returning Nigeria to Nigerians; farmers,<br />
@@ -169,17 +219,21 @@ const showVerify = provide<{ value: boolean }>("showVerify", showVerifyPhone);
           </p>
         </div>
         <!-- grid -->
-        <div class="pl-36 overflow-x-scroll myOverflow">
+        <div class="md:pl-36 pl-10 overflow-x-scroll myOverflow">
           <div class="flex space-x-4">
             <div class="relative shrink-0">
               <img
                 src="/images/png/sectionthreef.png"
                 alt=""
-                class="w-[27rem] h-[15rem]"
+                class="md:w-[27rem] md:h-[15rem] w-[20rem] h-[12rem]"
               />
-              <div class="absolute text-white top-[15%] left-[8%] space-y-20">
+              <div
+                class="absolute text-white top-[15%] left-[8%] md:space-y-20 space-y-10"
+              >
                 <div>
-                  <p class="font-extrabold text-[22px] leading-[34px]">
+                  <p
+                    class="font-extrabold text-[22px] md:leading-[34px] leading-5"
+                  >
                     Affordable, quality<br />health care
                   </p>
                 </div>
@@ -197,11 +251,15 @@ const showVerify = provide<{ value: boolean }>("showVerify", showVerifyPhone);
               <img
                 src="/images/png/sectionthrees.png"
                 alt=""
-                class="w-[27rem] h-[15rem]"
+                class="md:w-[27rem] md:h-[15rem] w-[20rem] h-[12rem]"
               />
-              <div class="absolute text-white top-[15%] left-[8%] space-y-20">
+              <div
+                class="absolute text-white top-[15%] left-[8%] md:space-y-20 space-y-10"
+              >
                 <div>
-                  <p class="font-extrabold text-[22px] leading-[34px]">
+                  <p
+                    class="font-extrabold text-[22px] md:leading-[34px] leading-5"
+                  >
                     Quality Education <br />beyond high school
                   </p>
                 </div>
@@ -219,9 +277,11 @@ const showVerify = provide<{ value: boolean }>("showVerify", showVerifyPhone);
               <img
                 src="/images/png/sectionthreel.png"
                 alt=""
-                class="w-[27rem] h-[15rem]"
+                class="md:w-[27rem] md:h-[15rem] w-[20rem] h-[12rem]"
               />
-              <div class="absolute text-white top-[15%] left-[8%] space-y-20">
+              <div
+                class="absolute text-white top-[15%] left-[8%] md:space-y-20 space-y-10"
+              >
                 <div>
                   <p class="font-extrabold text-[22px]">Security</p>
                 </div>
@@ -238,9 +298,9 @@ const showVerify = provide<{ value: boolean }>("showVerify", showVerifyPhone);
         </div>
       </section>
       <!-- section 4 -->
-      <section class="pb-36 px-36 space-y-12">
-        <div class="space-y-10">
-          <p class="text-[47px] font-extrabold leading-[50px]">
+      <section class="md:pb-36 pb-20 md:px-36 px-10 md:space-y-12 space-y-7">
+        <div class="md:space-y-10 space-y-7">
+          <p class="md:text-[47px] text-[40px] font-extrabold leading-[50px]">
             This is our opportunity to<br />
             take back our country
           </p>
@@ -253,15 +313,19 @@ const showVerify = provide<{ value: boolean }>("showVerify", showVerifyPhone);
           </p>
         </div>
         <!-- grid -->
-        <div class="grid grid-cols-2 gap-3">
+        <div class="grid md:grid-cols-2 gap-3">
           <div class="relative shrink-0">
             <img src="/images/png/sectionfourf.png" alt="" class="w-full" />
             <div class="absolute text-white top-[5%] left-[8%] space-y-20">
               <div>
-                <p class="font-extrabold text-[35px] text-[#BFB2B2]">
+                <p
+                  class="font-extrabold md:text-[35px] text-[22px] text-[#BFB2B2]"
+                >
                   Watch now
                 </p>
-                <p class="leading-[48px] font-extrabold text-[50px]">
+                <p
+                  class="md:leading-[48px] leading-[30px] font-extrabold md:text-[50px] text-[32px]"
+                >
                   Behind the scenes<br />and make off<br />
                   process
                 </p>
@@ -278,10 +342,14 @@ const showVerify = provide<{ value: boolean }>("showVerify", showVerifyPhone);
             <img src="/images/svg/sectionfours.svg" alt="" class="w-full" />
             <div class="absolute text-white top-[5%] left-[8%]">
               <div>
-                <p class="font-extrabold text-[35px] text-[#BFB2B2]">
+                <p
+                  class="font-extrabold md:text-[35px] text-[22px] text-[#BFB2B2]"
+                >
                   Watch now
                 </p>
-                <p class="leading-[48px] font-extrabold text-[50px]">
+                <p
+                  class="md:leading-[48px] leading-[30px] font-extrabold md:text-[50px] text-[32px]"
+                >
                   Unite for <br />
                   better future
                 </p>
@@ -299,10 +367,16 @@ const showVerify = provide<{ value: boolean }>("showVerify", showVerifyPhone);
           <div
             class="absolute bg-[rgba(0,0,0,0.6)] w-full h-full top-0 left-0 z-30"
           ></div>
-          <img src="/images/svg/sectionfourl.svg" alt="" class="w-full" />
+          <img
+            src="/images/svg/sectionfourl.svg"
+            alt=""
+            class="md:w-full w-screen md:h-auto h-[20rem]"
+          />
           <div class="absolute z-40 text-white top-[8%] left-[8%] space-y-20">
             <div>
-              <p class="leading-[48px] font-extrabold text-[50px]">
+              <p
+                class="md:leading-[48px] leading-[30px] font-extrabold md:text-[50px] text-[32px]"
+              >
                 “Nigeria is not a producing<br />
                 country. The cumulative effect<br />
                 of what we are suffering today<br />
@@ -312,13 +386,17 @@ const showVerify = provide<{ value: boolean }>("showVerify", showVerifyPhone);
           </div>
 
           <div class="absolute z-40 left-[8%] bottom-[8%]">
-            <p class="text-[34px] font-extrabold text-white">Obi|Datti 2023</p>
-            <p class="text-[34px] font-extrabold text-[#7F7F7F]">
+            <p class="md:text-[34px] text-[22px] font-extrabold text-white">
+              Obi|Datti 2023
+            </p>
+            <p class="md:text-[34px] text-[22px] font-extrabold text-[#7F7F7F]">
               Labour Party
             </p>
           </div>
           <div class="absolute z-40 bottom-[16%] right-[8%]">
-            <p class="flex text-[34px] font-extrabold text-[#7F7F7F]">
+            <p
+              class="flex md:text-[34px] text-[22px] font-extrabold text-[#7F7F7F]"
+            >
               Volunteer
               <span
                 ><img
